f4a5ac5419d61206b933b34a7b3efca4771d6088
Brandon Mitchell <git@bmitch.net>
2019-01-26 15:41:04 -0500

    Dropping capabilities

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-docker run -it --rm -p 127.0.0.1:6666:6666 golang-demo:${1:-latest}
+docker run -it --rm \
+  --cap-drop NET_RAW --cap-drop KILL \
+  -p 127.0.0.1:6666:6666 golang-demo:${1:-latest}
 
